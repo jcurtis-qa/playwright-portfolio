8f86cf4ba7624f06266177d08e0d0406439a93b9
John Curtis <jonathan.curtis@sphinxandrose.com>
2026-07-11 15:18:46 -0400

    Added conftest and pages __init__ for future definitions. Added class definition for LoginPage to reduce repetition. Updated test_login to utilize new LoginPage class.

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import Page
+
+class LoginPage:
+    URL = "https://www.saucedemo.com"
+
+    def __init__(self, page: Page):
+        # Define references to commonly used locators
+        self.page = page
+        self.username = page.locator('[data-test="username"]')
+        self.password = page.locator('[data-test="password"]')
+        self.login_button = page.locator('[data-test="login-button"]')
+
+        self.title = page.locator('[data-test="title"]')
+
+        self.error = page.locator('[data-test="error"]')
+
+    def load(self):
+        self.page.goto(self.URL)
+
+    def login(self, username: str, password: str):
+        self.username.fill(username)
+        self.password.fill(password)
+        self.login_button.click()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,19 +1,22 @@
 import pytest
+from pages.login_page import LoginPage
+
 from playwright.sync_api import Page, expect
 
 # Tests for successful login of a standard user
 def test_standard_user_can_log_in(page: Page):
+    # Create the login page object for this test's browser page
+    login_page = LoginPage(page)
+
     # Navigate to the test page
-    page.goto("https://www.saucedemo.com")
+    login_page.load()
 
     # Log in using standard user's credentials
-    page.locator('[data-test="username"]').fill("standard_user")
-    page.locator('[data-test="password"]').fill("secret_sauce")
-    page.locator('[data-test="login-button"]').click()
-
+    login_page.login('standard_user', 'secret_sauce')
+    
     # Ensure we have navigated to the inventory page
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
-    expect(page.locator('[data-test="title"]')).to_have_text("Products")
+    expect(login_page.title).to_have_text("Products")
 
 @pytest.mark.parametrize(
         "username,password,expected_error",
@@ -27,17 +30,17 @@ def test_standard_user_can_log_in(page: Page):
         ids=["locked-out-user", "incorrect-password", "unknown-user", "empty-username", "empty-password"]
 )
 
-# Tests for preventing login of a locked out user
+# Tests for expected login failure states
 def test_login_failures(page: Page, username, password, expected_error):
+    # Create the login page object for this test's browser page
+    login_page = LoginPage(page)
+    
     # Navigate to the test page
-    page.goto("https://www.saucedemo.com")
+    login_page.load()
 
     # Log in using error state user credentials
-    page.locator('[data-test="username"]').fill(username)
-    page.locator('[data-test="password"]').fill(password)
-    page.locator('[data-test="login-button"]').click()
+    login_page.login(username, password)
     
     # Ensure the error element is visible, and contains expected text
-    error = page.locator('[data-test="error"]')
-    expect(error).to_be_visible()
-    expect(error).to_have_text(expected_error)
+    expect(login_page.error).to_be_visible()
+    expect(login_page.error).to_have_text(expected_error)
