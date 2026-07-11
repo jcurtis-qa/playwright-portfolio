3003f47ce229dc6e1f0c551f070b6bdb2988e809
John Curtis <jonathan.curtis@sphinxandrose.com>
2026-07-11 17:06:40 -0400

    Replaced per-test login boilerplate with a fixture logged_in_page. Added ruff for linting/formatting.

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,31 +1,18 @@
-import pytest
-
-from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 
 from playwright.sync_api import Page, expect
 
-def test_product_count(page: Page):
-    # For demonstration purposes, login using the LoginPage class
-    login_page = LoginPage(page)
-    login_page.load()
-    login_page.login('standard_user','secret_sauce')
-
-    # Reframe context as an InventoryPage, now logged in
-    inventory_page = InventoryPage(page)
+def test_product_count(logged_in_page: Page):
+    # Create new inventory page from a logged in page
+    inventory_page = InventoryPage(logged_in_page)
     
     # Expect product count to match inventory list
     expect(inventory_page.inventory_items).to_have_count(6)
 
 
-def test_sort_ascending(page: Page):
-    # For demonstration purposes, login using the LoginPage class
-    login_page = LoginPage(page)
-    login_page.load()
-    login_page.login('standard_user','secret_sauce')
-
-    # Reframe context as an InventoryPage, now logged in
-    inventory_page = InventoryPage(page)
+def test_sort_ascending(logged_in_page: Page):
+    # Create new inventory page from a logged in page
+    inventory_page = InventoryPage(logged_in_page)
 
     # Sort items by ascending price
     inventory_page.select_sort('Price (low to high)')
